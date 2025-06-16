f4a37463cfaef669bf5bc12fdad9117257a3cf8a
Karan Gupta <kkg242003@gmail.com>
2025-06-16 14:46:20 +0530

    initial commit

---
+++ init/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({...obj,owner:"681b06e1ce02fb2db0b13320"}));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
